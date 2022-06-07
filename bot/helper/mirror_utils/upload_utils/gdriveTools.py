411f5efd4a6851c47cb8955d667a904d475dcd49
vegeteria <105584827+vegeteria@users.noreply.github.com>
2022-06-07 23:13:26 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -164,7 +164,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by Jarvis',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -434,7 +434,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Uploaded by Jarvis",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
@@ -679,7 +679,7 @@ class GoogleDriveHelper:
         for content in telegraph_content:
             path.append(
                 telegraph.create_page(
-                    title='Mirror-Leech-Bot Drive Search',
+                    title='Jarvis Drive Search',
                     content=content
                 )["path"]
             )
